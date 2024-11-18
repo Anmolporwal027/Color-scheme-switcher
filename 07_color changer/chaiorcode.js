72630417aad5a9588a01c1035da655e6b4b54a85
anmolporwal027@gmail.com <anmolporwal027@gmail.com>
2024-11-18 17:43:07 +0530

    first commit

---
+++ 07_color changer/chaiorcode.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+// console.log(button);
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        // console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = "grey"
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = "white"
+        }
+        if(e.target.id === 'purple'){
+            body.style.backgroundColor = "purple"
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = "blue"
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = "yellow"
+        }
+        
+    })
+})
+
+
+
